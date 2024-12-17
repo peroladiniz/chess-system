e209d41fe0bbe6525e04751e1f68d2789635d919
Perola Diniz <peroladnizz@hotmail.com>
2024-12-17 16:47:00 -0300

    Chess layer and printing the board

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,16 @@
 package boardgame;
 
 public class Piece {
+	
+	protected Position positon;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+	}
 
+	protected Board getBoard() {
+		return board;
+	}
+	
 }
